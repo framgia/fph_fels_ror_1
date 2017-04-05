3c6cc0d6f2c26fbf74c7358f54927e0a2293ad16
Inah <inahsanjuan@gmail.com>
2017-04-05 15:37:36 +0800

    [FPH-109664] User roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -10,5 +13,9 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def add_role(role_object)
+    self.user_roles.create(role: role_object)
   end
 end
